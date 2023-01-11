fe721f428fb8e92de10f13317c8038929ef5b8af
Zeddzuruzuranda <raphomelvo@gmail.com>
2023-01-11 14:38:22 +0300

    assignmsent complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,36 @@
 // Code EyesOnMe Component Here
+import React from "react";
+function EyesOnMe() {
+   function focus() {
+      console.log('good!')
+   }
+   function blur() {
+      console.log('Hey! Eyes on me!')
+   }
+   return(
+      <button onFocus={focus} onBlur={blur}>
+         EyesOnMe</button>
+   );
+}
+export default EyesOnMe;
+
+
+
+
+
+
+
+
+// import React from "react";
+// function EyesOnMe(){
+//    function focus(){
+//       console.log ('Good!')
+//    }
+//    function blur(){
+//       console.log("Hey! Eyes on me!")
+//    }
+//    return(
+//       <button onFocus={focus} onBlur={blur}>EyesOnMe</button>
+//    )
+//    }
+
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,11 @@
 // Code Keypad Component Here
 
-function Keypad (){
+function Keypad() {
+    function PasswordChange() {
+        console.log("Entering Password...");
+    }
     return (
-        <div></div>
+        <input type="password" onChange={PasswordChange} />
     )
 }
 
